893768532f9f2b710d3debd3a6c6218a02c49b9d
Shahad Puthukudi <shahadp@gmail.com>
2020-05-24 00:31:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber(current_line, new_person_in_line)
 {
   let new_person_current_line=`Welcome, ${new_person_in_line}. You are number ${current_line+1} in line.`;
+return new_person_current_line;
 }
