9ed6f4471c39fe94523a220068e66aa2aa849772
Shahad Puthukudi <shahadp@gmail.com>
2020-05-24 00:38:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-function takeANumber(current_line, new_person_in_line)
+function takeANumber(katzDeliLine, new_person_in_line)
 {
-  let new_person_current_line=`Welcome, ${new_person_in_line}. You are number ${current_line.length+1} in line.`;
-  current_line.push(new_person_in_line);
+  let new_person_current_line=`Welcome, ${new_person_in_line}. You are number ${katzDeliLine.length+1} in line.`;
+  katzDeliLine.push(new_person_in_line);
   console.log(new_person_current_line)
   return new_person_current_line
+}
+
+function nowServing(katzDeliLine)
+{
+  
 }
